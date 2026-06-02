9ce513261e47030d6bb2cb70e7f5db7ded34a45f
Govind Gore <govind.gore.tech@gmail.com>
2026-06-02 13:48:43 +0530

    Add files via upload

---
+++ C_Programming/Competitive/OddFactorial.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int Fact(int iNo)
+{
+   if (iNo<0)
+   {
+      iNo=-iNo;
+   }
+   
+   int iCnt =0;
+   int iFact=1;
+
+   for ( iCnt = iNo; iCnt >= 1; iCnt--)
+   {
+      if (iCnt%2!=0)
+      {
+         iFact= iCnt*iFact;
+      }
+      
+      
+   }
+   return iFact;
+   
+   
+    
+}   
+int main()
+ {
+    int iValue = 0;
+     int iRet = 0;
+    printf ("Enter the Number : ");
+    scanf("%d",&iValue);
+
+   iRet = Fact(iValue);
+
+   printf("Odd Factorial Number is: %d",iRet);
+    
+
+
+    return 0;
+ }
