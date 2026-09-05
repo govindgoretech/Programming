3625c6bd05f7e8edd49e83c7e65b29d273c2ab55
ovind ore <goregovind566@gmail.com>
2026-09-05 12:13:37 +0530

    Add Python practice programs

---
+++ Python/Practice/TypeDemo.py
@@ -0,0 +1,17 @@
+no = 11             # int
+marks= 90.78        # float
+value = 8 +5j       # Complex
+
+print(type(no))
+print(type(marks))
+print(type(value))
+
+gender = True       #Bool
+
+print(gender)
+print(type(gender))
+
+Demo = None
+
+print(type(Demo))
+print(Demo)
