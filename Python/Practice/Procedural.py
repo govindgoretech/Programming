cac77be59f23005ad6a96c810115bbe93bfe959d
ovind ore <goregovind566@gmail.com>
2026-09-07 18:04:15 +0530

    Automatic daily update

---
+++ Python/Practice/Procedural.py
@@ -0,0 +1,19 @@
+def Addition(No1, No2):
+    Ans = No1 + No2
+    return Ans
+
+def Substraction(No1, No2):
+    Ans = No1 - No2
+    return Ans
+
+print("Enter first number : ")
+Value1 = int(input())
+
+print("Enter second number : ")
+Value2 = int(input())
+
+Ret = Addition(Value1,Value2)
+print("Addition is : ",Ret)
+
+Ret = Substraction(Value1,Value2)
+print("Substraction is : ",Ret)
